4bf48d4ead437dff5833af069ea916fa74fb8118
jgunn <j.s.gunn22@gmail.com>
2023-08-15 14:23:30 -0700

    prints all shapes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const shapes = require("./lib/shapes.js");
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 // const rect = new shapes.Rectangle(`SVG`, "white", `red`);
 // const cir = new shapes.Circle("PIX", "white", "blue");
@@ -39,25 +40,48 @@ function init() {
   async function print() {
     let a = await answers();
 
+    let fileName = a.name;
     let name = a.name;
     let textColor = a.textColor;
     let fill = a.fill;
+    let shapeCode;
+    let textSize = 60;
+    let textPosition;
 
     let shape = questions[2].choices;
+
     switch (a.shape) {
       case shape[0]:
-        const circle = new shapes.Circle(name, textColor, fill);
-        circle.render();
+        const circle = new shapes.Circle(fill);
+        shapeCode = circle.render();
+        textPosition = `x="150" y="122" text-anchor="middle"`;
         break;
       case shape[1]:
-        const rectangle = new shapes.Rectangle(name, textColor, fill);
-        rectangle.render();
+        const rectangle = new shapes.Rectangle(fill);
+        shapeCode = rectangle.render();
+        textPosition = `x="0" y="0"`;
+        name = `<tspan x="16.5" y="104">${name}</tspan>`;
+        break;
       case shape[2]:
-        const triangle = new shapes.Triangle(name, textColor, fill);
-        triangle.render();
+        const triangle = new shapes.Triangle(fill);
+        shapeCode = triangle.render();
+        textPosition = `x="150" y="144" text-anchor="middle"`;
+        textSize = 48;
       default:
         break;
     }
+
+    fs.writeFile(
+      `./examples/${fileName}.svg`,
+      `<svg width='160' height='160'>
+        ${shapeCode}
+        <text ${textPosition} font-size="${textSize}" fill="${textColor}">
+        ${name}
+         </text>
+        </svg>`,
+      (err) =>
+        err ? console.log(err) : console.log("Saved to examples folder")
+    );
   }
 
   print();
