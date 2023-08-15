020ee3b67b013cdc9ad7a49437687eb0639b01f4
jgunn <j.s.gunn22@gmail.com>
2023-08-15 12:33:27 -0700

    prints rectangle

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const shapes = require("./lib/shapes.js");
 
 const rect = new shapes.Rectangle(`SVG`, `red`);
 const cir = new shapes.Circle("PIX", "blue");
+const tri = new shapes.Triangle("TUC", "green");
 
 rect.print();
 cir.print();
+tri.print();
